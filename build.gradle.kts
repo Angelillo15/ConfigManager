6273e4aaa4622a8565aaf4b9fb9fd0bc165b9a50
Angelillo15 <angelrizosrubios@gmail.com>
2022-10-02 12:14:57 +0200

    Added maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "4.0.4"
+    id("maven-publish")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "es.angelillo15"
+version = "1.1"
 
 repositories {
     mavenCentral()
@@ -20,4 +21,16 @@ tasks.shadowJar {
     archiveFileName.set("ConfigManager.jar")
     relocate("org.simpleyaml", "es.angelillo15.minearte.libs.simpleyaml")
     relocate("org.yaml", "es.angelillo15.minearte.libs.yaml")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "es.angelillo15"
+            artifactId = "ConfigManager"
+            version = "1.1"
+
+            from(components["java"])
+        }
+    }
 }
